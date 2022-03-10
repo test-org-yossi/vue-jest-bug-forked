024284571a860d06301e21e2600376838d517a70
Tal Koren <tal@swimm.io>
2022-03-10 16:10:00 +0200

    initial commit

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -7,6 +7,8 @@ describe("HelloWorld.vue", () => {
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
+
+    console.log(wrapper.html());
     expect(wrapper.text()).toMatch(msg);
   });
 });
